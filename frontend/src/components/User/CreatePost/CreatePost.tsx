6ca95d2cddc77a96e73737843a29154f2f0ef570
rafael <rafaelmagalhaes1@live.com>
2024-02-03 19:16:25 -0300

    feat: component CreatePost refactored

--- frontend/src/components/User/CreatePost/CreatePost.tsx
+++ frontend/src/components/User/CreatePost/CreatePost.tsx
@@ -1,73 +1,31 @@
-import Swal from "sweetalert2";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { CreatePostButton, CreatePostContainer } from "./Style";
-import { findCategories } from "../../../services/category.service";
+import { postHook } from "../../../hooks/postHook";
+import { FaArrowLeft } from "react-icons/fa";
 
 export const CreatePost = () => {
   const { id } = useParams();
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [categories, setCategories] = useState([]);
-  const [selectedCategories, setSelectedCategories] = useState<any>([]);
-  
+  const {
+    title,
+    setTitle,
+    content,
+    setContent,
+    categories,
+    selectedCategories,
+    fetchCategories,
+    handleCreatePost,
+    handleCheckboxChange
+  } = postHook(Number(id));
+
   useEffect(() => {
-    const fetchCategories = async () => {
-      const data = await findCategories();
-      setCategories(data);
-    }
     fetchCategories();
   }, []);
 
-  const handleCreatePost = async () => {
-    const res = await fetch("http://localhost:3001/post", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("@Auth:access_token")}`,
-      },
-      body: JSON.stringify({
-        id: id,
-        title: title,
-        content: content,
-        categoryIds: selectedCategories.map(Number),
-      }),
-    });
-    const data = await res.json();
-
-    if (res.status === 201) {
-      Swal.fire({
-        icon: "success",
-        title: "Post created",
-        showConfirmButton: false,
-        timer: 1500,
-      });
-      setTitle("");
-      setContent("");
-      setSelectedCategories([]);
-    } else {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: data.message,
-      });
-    }
-  };
-
-  const handleCheckboxChange = (event: any) => {
-    const value = Number(event.target.value);
-    if (event.target.checked) {
-      setSelectedCategories([...selectedCategories, value]);
-    } else {
-      setSelectedCategories(selectedCategories.filter((category: any) => category !== value));
-    }
-  };
-
   return (
     <CreatePostContainer>
-      <Link to="/">Back</Link>
-      <br />
+      <Link to="/"><FaArrowLeft size={15} /> Back</Link>
       <h1>Create new post</h1>
       <input
         type="text"
