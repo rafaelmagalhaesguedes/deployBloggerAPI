b6be71e846680708a743055e68e263180872b866
rafael <rafaelmagalhaes1@live.com>
2024-02-03 19:18:52 -0300

    feat: create function createCategory on category.service

--- frontend/src/services/category.service.ts
+++ frontend/src/services/category.service.ts
@@ -1,3 +1,23 @@
+export const createCategory = async (name: string) => {
+  const res = await fetch("http://localhost:3001/categories", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("@Auth:access_token")}`,
+    },
+    body: JSON.stringify({
+      name: name,
+    }),
+  });
+
+  if (res.status !== 201) {
+    const data = await res.json();
+    throw new Error(data.message);
+  }
+
+  return res.json();
+}
+
 export const findCategories = async () => {
   const res = await fetch("http://localhost:3001/categories", {
     method: "GET",
