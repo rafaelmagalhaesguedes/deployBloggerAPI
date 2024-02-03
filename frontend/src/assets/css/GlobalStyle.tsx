4a5a0cd1e4987d3043ab9cb97b4401211c37eb6f
rafael <rafaelmagalhaes1@live.com>
2024-02-03 17:31:34 -0300

    feat: add font style on global style

--- frontend/src/assets/css/GlobalStyle.tsx
+++ frontend/src/assets/css/GlobalStyle.tsx
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  // import poppins font
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+
   * {
     margin: 0;
     padding: 0;
@@ -10,6 +13,10 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: Open-Sans, Helvetica, Sans-Serif;
     background: #f2f2f2;
+  }
+
+  h1, span {
+    font-family: 'Poppins', sans-serif;
   }
 
   table {
