ff110e9d6f49149a4823d7ff71967c0f4f17011e
rafael <rafaelmagalhaes1@live.com>
2024-02-04 12:43:26 -0300

    fix: css categories value

--- frontend/src/components/Posts/SinglePost/SinglePost.tsx
+++ frontend/src/components/Posts/SinglePost/SinglePost.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { findPostById } from "../../../services/post.service";
 import formatDate from "../../../utils/formatDate";
-import { ContainerPost, Content, MenuBody, PostCard, Published, Title } from "./Style";
+import { Category, ContainerPost, Content, MenuBody, PostCard, Published, Title } from "./Style";
 import { PostType } from "../../../types/types";
 import { Link } from "react-router-dom";
 import { FaArrowLeft, FaPlusCircle } from "react-icons/fa";
@@ -41,11 +41,16 @@ export const SinglePost = () => {
             <span>Published in: {formatDate(post.published)}</span>
           </Published>
           <Content>{post.content}</Content>
-          <div>
-            <span>Category: {post.categories?.map((category: any) => (
-              <span className="category-item" key={category.id}>{category.name}</span>
-            ))}</span>
-          </div>
+          <Category>
+            <span>
+              {post.categories?.map((category: any) => (
+              <span key={category.id}>
+                <span className="category-item" >{category.name}</span>
+                {' '}
+              </span>
+            ))}
+            </span>
+          </Category>
         </PostCard>
       ))}
     </ContainerPost>
--- frontend/src/components/Posts/SinglePost/Style.tsx
+++ frontend/src/components/Posts/SinglePost/Style.tsx
@@ -12,27 +12,27 @@ export const ContainerPost = styled.div`
 `;
 
 export const MenuBody = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5px;
+  margin-bottom: 20px;
+  background-color: #f2f2f2;
+  padding: 10px;
+  border-radius: 5px;
+
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+
+  span {
     display: flex;
-    justify-content: space-between;
     align-items: center;
-    gap: 5px;
-    margin-bottom: 20px;
-    background-color: #f2f2f2;
-    padding: 10px;
-    border-radius: 5px;
-
-    a {
-        text-decoration: none;
-        color: #000;
-    }
-
-    span {
-        display: flex;
-        align-items: center;
-        gap: 7px;
-        cursor: pointer;
-        font-size: 16px;
-    }
+    gap: 7px;
+    cursor: pointer;
+    font-size: 16px;
+  }
 }`;
 
 export const PostCard = styled.div`
@@ -47,15 +47,30 @@ export const Title = styled.h2`
 `;
 
 export const Published = styled.div`
-    display: flex;
-    justify-content: start;
-    margin: 0 0 1rem 0;
-    font-size: 0.9rem;
-    gap: 3rem;
-    `;
+  display: flex;
+  justify-content: start;
+  margin: 0 0 1rem 0;
+  font-size: 0.9rem;
+  gap: 3rem;
+`;
 
 export const Content = styled.div`
-    margin: 0.5rem 0 2rem 0;
-    font-size: 1rem;
-    line-height: 1.5;
-    `;
+  margin: 0.5rem 0 2rem 0;
+  font-size: 1rem;
+  line-height: 1.5;
+  text-align: justify;
+`;
+
+export const Category = styled.div`
+  display: flex;
+  justify-content: start;
+  margin: 0 0 1rem 0;
+  font-size: 0.9rem;
+
+  .category-item {
+    background-color: #333;
+    color: #fff;
+    padding: 5px;
+    border-radius: 5px;
+  }
+`;
