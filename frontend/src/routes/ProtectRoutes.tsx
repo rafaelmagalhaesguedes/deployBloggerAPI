5805b6aad0b569036ade3a0bc4c517864895621c
rafael <rafaelmagalhaes1@live.com>
2024-02-04 12:30:45 -0300

    feat: add route /single-post/:id

--- frontend/src/routes/ProtectRoutes.tsx
+++ frontend/src/routes/ProtectRoutes.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CreatePost } from "../components/User/CreatePost/CreatePost";
 import { UserPosts } from "../components/User/UserPosts/UserPosts";
-import CreateCategory from "../components/Categories/CreateCategories";
+import Categories from "../components/Categories/Categories";
 import Layout from "../components/Layout/Layout";
 import Profile from "../pages/Profile/Profile";
 import { Home } from "../pages/Home/Home";
+import { SinglePost } from "../components/Posts/SinglePost/SinglePost";
 
 export const ProtectRoutes = () => {
   return (
@@ -12,8 +13,9 @@ export const ProtectRoutes = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/single-post/:id" element={<SinglePost />} />
           <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/create-category" element={<CreateCategory />} />
+          <Route path="/categories" element={<Categories />} />
           <Route path="/user-posts" element={<UserPosts />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<h1>Page Not Found</h1>} />
