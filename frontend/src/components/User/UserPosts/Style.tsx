7cdf930ad67128aa0419ac72ae4912662638bf9f
rafael <rafaelmagalhaes1@live.com>
2024-02-04 16:17:05 -0300

    refactor: UserPosts component

--- frontend/src/components/User/UserPosts/Style.tsx
+++ frontend/src/components/User/UserPosts/Style.tsx
@@ -6,79 +6,108 @@ export const UserPostsContainer = styled.div`
   margin-top: 20px;
   width: 80%;
   margin: 0 auto;
-  padding: 30px;
+
+  background-color: #fff;
+`;
+
+export const MenuBody = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   gap: 5px;
+  margin-bottom: 20px;
+  background-color: #f2f2f2;
+  padding: 10px;
+  border-radius: 5px;
 
-    div {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        gap: 5px;
-        margin-bottom: 20px;
-        background-color: #f2f2f2;
-        padding: 10px;
-        border-radius: 5px;
-    }
+  span {
+    display: flex;
+    align-items: center;
+    gap: 7px;
+    cursor: pointer;
+    font-size: 16px;
+  }
 
-    span {
-        display: flex;
-        align-items: center;
-        gap: 7px;
-        cursor: pointer;
-        font-size: 16px;
-    }
+  a {
+    border-radius: 5px;
+    text-decoration: none;
+    color: #000;
+    font-weight: 400;
+  }
+`;
 
-    a {
-        border-radius: 5px;
-        text-decoration: none;
-        color: #000;
-        font-weight: 400;
-    }
+export const Post = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 
-    background-color: #fff;
-    border-top: 1px solid #ccc;
+  padding: 20px;
+  border-bottom: 1px solid #f2f2f2;
+  margin: 0 0 1rem 0;
 
-    h2 {
-        margin-bottom: 20px;
-    }
+  background-color: #f2f2f2;
+  border-radius: 5px;
 
-    table {
-        width: 100%;
-        border-collapse: collapse;
-        border: 1px solid #000;
+  margin: 0 1rem 1rem 1rem;
+`;
 
-        th, td {
-            border: 1px solid #000;
-            padding: 10px;
-            text-align: center;
-        }
+export const PostHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-        .buttons {
-            display: flex;
-            gap: 5px;
-            justify-content: center;
+  button {
+    padding: 5px 10px;
+    border-radius: 5px;
+    background-color: #000;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+  }
 
-            button {
-                padding: 5px 10px;
-                border: none;
-                border-radius: 5px;
-                cursor: pointer;
-                background-color: #000;
-                color: #fff;
-            }
-        }
+  a {
+    color: #fff;
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
 
-        th {
-            background-color: #000;
-            color: #fff;
-        }
+export const PostTitle = styled.h3`
+  font-size: 1.8rem;
+`;
 
-        tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
+export const PostContent = styled.p`
+  font-size: 16px;
+  text-align: justify;
 
-        tr:hover {
-            background-color: #ddd;
-        }
-    }
-`; 
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+`;
+
+export const PostFooter = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  gap: 25px;
+  font-size: 14px;
+
+  div {
+    display: flex;
+    gap: 5px;
+  }
+`;
+
+export const PostCategory = styled.div`
+  display: flex;
+  gap: 5px;
+  margin-top: 10px;
+
+  span {
+    padding: 5px 10px;
+    border-radius: 5px;
+    background-color: #000;
+    color: #fff;
+  }
+`;
--- frontend/src/components/User/UserPosts/UserPosts.tsx
+++ frontend/src/components/User/UserPosts/UserPosts.tsx
@@ -1,91 +1,60 @@
 import { useEffect } from "react";
 import { useAuth } from "../../../context/auth";
 import { UserType } from "../../../types/types";
-import { UserPostsContainer } from "./Style";
+import { UserPostsContainer, Post, PostTitle, PostContent, PostFooter, PostHeader, PostCategory } from "./Style";
 import { userHook } from "../../../hooks/userHook";
 import { Link } from "react-router-dom";
 import formatDate from "../../../utils/formatDate";
-import { FaArrowLeft, FaPlusCircle } from "react-icons/fa";
+import { FaArrowLeft, FaEdit, FaPlusCircle, FaTrash } from "react-icons/fa";
+import trimContent from "../../../utils/trimContent";
+import { MenuBody } from "../EditPost/Style";
 
 export const UserPosts = () => {
   const { user } = useAuth() as { user: UserType };
   const { id } = user;
   const {
     userPosts,
-    editingPostId,
-    editingPostData,
     fetchUserPosts,
-    handleEditUserPost,
     handleDeleteUserPost,
-    setEditingPostData
-  } = userHook(id);
+  } = userHook();
 
   useEffect(() => {
-    fetchUserPosts();
-  }, []);
+    fetchUserPosts(id);
+  }, [id]);
 
   return (
     <UserPostsContainer>
-      <div>
+      <MenuBody>
         <Link to="/"><FaArrowLeft size={15} /> Back</Link>
         <h3>My Posts</h3>
         <Link to="/create-post"><span><p>New post</p> <FaPlusCircle size={20} /></span></Link>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Content</th>
-            <th>Author</th>
-            <th>Published</th>
-            <th>Updated</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {userPosts && userPosts.length > 0 ? (
-            userPosts.map((post: any) => (
-              <tr key={post.id}>
-                <td>
-                  {editingPostId === post.id ? (
-                    <input
-                      value={editingPostData?.title}
-                      onChange={(e) => setEditingPostData({ ...editingPostData, title: e.target.value })}
-                    />
-                  ) : (
-                    post.title
-                  )}
-                </td>
-                <td>
-                  {editingPostId === post.id ? (
-                    <input
-                      value={editingPostData?.content}
-                      onChange={(e) => setEditingPostData({ ...editingPostData, content: e.target.value })}
-                    />
-                  ) : (
-                    post.content
-                  )}
-                </td>
-                <td>{post.user.displayName}</td>
-                <td>{formatDate(post.published)}</td>
-                <td>{formatDate(post.updated)}</td>
-                <td>
-                  <div className="buttons">
-                    <button
-                      onClick={() => handleEditUserPost(post.id)}>{editingPostId === post.id ? 'Save' : 'Edit'}
-                    </button>
-                    <button onClick={() => handleDeleteUserPost(post.id)}>Delete</button>
-                  </div>
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={6}>You have not published any posts yet.</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      </MenuBody>
+      {userPosts && userPosts.length > 0 ? (
+        userPosts.map((post: any) => (
+          <Post key={post.id}>
+            <PostHeader>
+              <PostTitle>{post.title}</PostTitle>
+              <div>
+                <button><Link to={`/edit-post/${post.id}`}>Edit</Link></button>
+                <button onClick={() => handleDeleteUserPost(post.id)}>Delete</button>
+              </div>
+            </PostHeader>
+            <PostFooter>
+              <div>Author: {post.user.displayName}</div>
+              <div>Published in: {formatDate(post.published)}</div>
+              <div>Updated in: {formatDate(post.updated)}</div>
+            </PostFooter>
+            <PostContent><Link to={ `/single-post/${post.id}` }>{trimContent(post.content, 50)}</Link></PostContent>
+            <PostCategory>
+              {post.categories.map((category: any) => (
+                <span key={category.id}>{category.name}</span>
+              ))}
+            </PostCategory>
+          </Post>
+        ))
+      ) : (
+        <p>You have not published any posts yet.</p>
+      )}
     </UserPostsContainer>
   );
 };
