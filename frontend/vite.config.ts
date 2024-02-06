05785ebac0acd93ecb631ec41bcaea02eff8497d
rafael <rafaelmagalhaes1@live.com>
2024-02-06 18:06:45 -0300

    fix: optimizeDeps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: '/src/main.js'
-    }
+  optimizeDeps: {
+    entries: [],
   }
 })
