de5a950677a04b2809c8518ad7d00322ee854618
rafael <rafaelmagalhaes1@live.com>
2024-02-06 12:32:09 -0300

    fix: access control

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,17 +3,10 @@ const cors = require('cors');
 const routers = require('./routes');
 
 const app = express();
-app.use(cors({ origin: '*' }));
+app.use(cors());
 app.use(express.json());
 app.use(routers);
 
 app.get('/', (_request, response) => response.send('Healthy check OK!'));
-
-app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
-  res.header('Access-Control-Allow-Headers', '*');
-  next();
-});
 
 module.exports = app;
