361a61b68322db1dcebdba6fd59154bb95573875
rafael <rafaelmagalhaes1@live.com>
2024-02-06 10:10:27 -0300

    fix: vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: '/frontend/index.js',
-    },
-  },
   optimizeDeps: {
     include: ['react', 'react-dom'],
   },
