a0dffd2f11849a9d5a4c81185a231285b9e5b3e5
rafael <rafaelmagalhaes1@live.com>
2024-02-06 13:34:17 -0300

    fix: app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,18 +3,19 @@ const cors = require('cors');
 const routers = require('./routes');
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(routers);
 
-app.get('/', (_request, response) => response.send('Healthy check OK!'));
+app.use(cors());
 
 app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
+
+app.use(express.json());
+app.use(routers);
+
+app.get('/', (_request, response) => response.send('Healthy check OK!'));
 
 module.exports = app;
