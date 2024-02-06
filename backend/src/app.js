0e3fbed575a87a2e6551fd6a2acaee1a38ed6f77
rafael <rafaelmagalhaes1@live.com>
2024-02-06 11:34:28 -0300

    feat: cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const routers = require('./routes');
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(routers);
+app.use(cors());
 
 app.get('/', (_request, response) => response.send('Healthy check OK!'));
 
