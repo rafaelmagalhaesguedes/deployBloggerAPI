4a0f7bb91a5d5b4995ffbad6adeb418840a0eba3
rafael <rafaelmagalhaes1@live.com>
2024-02-06 19:44:38 -0300

    fix: vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['axios', 'react', 'react-dom'],
-  },
-  build: {
-    commonjsOptions: {
-      include: ['axios', 'react', 'react-dom'],
-    },
-  },
 })
