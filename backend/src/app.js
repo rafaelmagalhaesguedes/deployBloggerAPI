a188408699c33ca0a99472c0bd6b5905b1cb2b45
rafael <rafaelmagalhaes1@live.com>
2024-02-06 12:00:30 -0300

    fix: app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,12 @@ app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(routers);
 
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // replace '*' with your domain if you want to restrict access
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', (_request, response) => response.send('Healthy check OK!'));
 
 module.exports = app;
