861f30bf4f9b03ca722a7ac31f257bcc71bd1816
rafael <rafaelmagalhaes1@live.com>
2024-02-06 21:33:19 -0300

    fix: app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,12 @@ const routers = require('./routes');
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cors());
 
 app.options('*', cors());
 
-app.use(express.json());
 app.use(routers);
 
 app.get('/', (_request, response) => response.send('Healthy check OK!'));
