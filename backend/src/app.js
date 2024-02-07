7b43342d1d40f82737d3e4f361a3f00f4a50efb2
rafael <rafaelmagalhaes1@live.com>
2024-02-06 21:37:51 -0300

    feat: create middleware to headers

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,17 @@ const routers = require('./routes');
 
 const app = express();
 
-app.use(express.json());
-
 app.use(cors());
 
-app.options('*', cors());
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 
+app.use(express.json());
 app.use(routers);
 
 app.get('/', (_request, response) => response.send('Healthy check OK!'));
